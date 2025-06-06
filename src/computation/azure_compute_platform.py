a968d7816618e3667a9d90304b9a0dbf4dc83e96
Santiago Martínez Willi <santiago30mw@gmail.com>
2025-06-07 00:07:54 +0200

    wip: preprocessing job now runs in AML but it is crashing due to memory overflow

--- src/computation/azure_compute_platform.py
+++ src/computation/azure_compute_platform.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Any
-from azure.ai.ml import MLClient, command
+from azure.ai.ml import MLClient, command, Input, Output
+from azure.ai.ml.constants import AssetTypes, InputOutputModes
 from azure.identity import DefaultAzureCredential
 from .job_type import JobType
 from .job_id import JobID
@@ -9,33 +10,49 @@ from .compute_platform import ComputePlatform
 class AzureComputePlatform(ComputePlatform):
     def __init__(
         self,
-        subscription_id: str,
-        resource_group: str,
-        workspace_name: str,
-        compute_name: str,
-        environment_name: str,
+        subscription_id: str  = None,
+        resource_group: str   = None,
+        workspace_name: str   = None,
+        compute_name: str     = None,
+        environment_name: str = None,
     ):
-        self._ml_client = MLClient(
-            DefaultAzureCredential(), subscription_id, resource_group, workspace_name
-        )
-        self._compute_name = compute_name
-        self._environment_name = environment_name
+        if subscription_id and resource_group and workspace_name and compute_name and environment_name:
+            self._ml_client = MLClient(
+                DefaultAzureCredential(), subscription_id, resource_group, workspace_name
+            )
+            self._compute_name = compute_name
+            self._environment_name = environment_name
+        else:
+            # For AML jobs
+            self._ml_client = MLClient(DefaultAzureCredential())
 
     def queue_job(
         self, job_type: JobType, input_name: str, input_version: str, output_name: str
     ) -> JobID:
         src_path = str(Path(__file__).resolve().parents[1])
-
         job = command(
             code=src_path,
             command=(
-                "python src/computation/jobs/preprocessing_job.py "
-                f"--input-name {input_name} --input-version {input_version} "
-                f"--output-name {output_name}"
+                "python computation/jobs/preprocessing_job.py "
+                "--input-path ${{inputs.ppi_input}} "
+                "--output-dir ${{outputs.ppi_output}}"
             ),
             environment=self._environment_name,
             compute=self._compute_name,
             experiment_name="ppi_experiment",
+            inputs={
+                "ppi_input": Input(
+                    type=AssetTypes.URI_FOLDER, 
+                    path=f"{input_name}:{input_version}",
+                    mode=InputOutputModes.RO_MOUNT
+                )
+            },
+            outputs={
+                "ppi_output": Output(
+                    type=AssetTypes.URI_FOLDER, 
+                    mode=InputOutputModes.RW_MOUNT
+                )
+            },
         )
         returned = self._ml_client.jobs.create_or_update(job)
         return JobID(returned.name)
--- src/computation/jobs/preprocessing_job.py
+++ src/computation/jobs/preprocessing_job.py
@@ -1,29 +1,32 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[2]))
+
 import argparse
-from computation.azure_compute_platform import AzureComputePlatform
 from sequence_preprocessor import SequencePreprocessor
-from encoders.sequence_encoder import SequenceEncoder
+from encoders.numeric_sequence_encoder import NumericSequenceEncoder
 import mltable
 import pandas as pd
 
 def main(args):
-    platform = AzureComputePlatform()
+    print("-- Preprocessing job started --")
+    path = {'folder': args.input_path}
 
-    data_asset = platform.get_resource(name=args.input_name, version=args.input_version)
-    path = {'folder': data_asset.path}
-
-    table = mltable.from_delimited_files(paths=[path])
+    print("-- Reading input --")
+    table = mltable.from_parquet_files(paths=[path])
     df = table.to_pandas_dataframe()
 
-    encoder = SequenceEncoder()
-    preprocessor = SequencePreprocessor(encoder=encoder, max_length=1024)
+    print("-- Preprocess started --")
+    encoder = NumericSequenceEncoder()
+    preprocessor = SequencePreprocessor(encoder=encoder, max_length=1024, chunk_size=256)
     df_processed = preprocessor.process_dataframe(df)
 
-    df_processed.to_parquet("./outputs/processed.parquet", index=False)
+    print("-- Saving processed data --")
+    df_processed.to_parquet(Path(args.output_dir) / "processed.parquet", index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-name",    required=True)
-    parser.add_argument("--input-version", required=True)
-    parser.add_argument("--output-name",   required=True)
+    parser.add_argument("--input-path", required=True)
+    parser.add_argument("--output-dir", required=True)
     args = parser.parse_args()
     main(args)
